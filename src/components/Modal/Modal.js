119ba9cfa9cf12d594a448cd31b1fafacca57f51
Dieter Doman <dieter@jemstep.com>
2019-08-16 15:10:38 +0200

    Added different timings to enter and exit.

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './Modal.css';
 import Transition from "react-transition-group/esm/Transition";
 
+const animationTiming = {
+    enter: 400,
+    exit: 1000
+};
+
 const modal = (props) => {
     return (
-        <Transition in={props.show} mountOnEnter unmountOnExit timeout={300}>
+        <Transition in={props.show} mountOnEnter unmountOnExit timeout={animationTiming}>
             {state => {
                 const modalClasses = ['Modal', state === 'entering' ? 'ModalOpen' : state === 'exiting' ? 'ModalClosed' : null];
                 return (
